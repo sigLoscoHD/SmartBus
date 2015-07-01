f305bee150ce0ab68a4881d978f4c92a91eeee37
sigLoscoHD <matteo.cappella@studenti.unicam.it>
2015-07-01 22:08:26 +0200

    Inserito controllo password e ripeti password, sistemato sistema di cartelle (creata la cartella css, javascript e images), aggiustato stile modal

---
+++ SmartBus/SmartBus/web/javascript/controllo.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function controllo(){
+        var password=$("#pass").val();
+        var npassword=$("#npass").val();
+       
+	     if(password !== npassword)
+	      {
+	         $("#myModal").append("<div id='err-pass'class='alert alert-danger alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Errore</strong>: le password inserite non sono uguali!</div>");
+	            
+	            
+	            return false;
+	      	}
+      return true;
+}
