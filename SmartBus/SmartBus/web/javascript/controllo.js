e890191ec1b23d74312cf85f24a76f143d9bab56
sigLoscoHD <matteo.cappella@studenti.unicam.it>
2015-07-02 12:21:10 +0200

    Aggiunto effetto a scomparsa dell'alert, aggiunto jumpotron, spostato il login e il sign up a destra della navbar

--- SmartBus/SmartBus/web/javascript/controllo.js
+++ SmartBus/SmartBus/web/javascript/controllo.js
@@ -11,8 +11,9 @@ function controllo(){
        
 	     if(password !== npassword)
 	      {
-	         $("#myModal").append("<div id='err-pass'class='alert alert-danger alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Errore</strong>: le password inserite non sono uguali!</div>");
-	            
+	         $("#myModal").append("<div id='err-pass'class='alert alert-danger alert-dismissible fade' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Errore</strong>: le password inserite non sono uguali!</div>");
+	           $("#err-pass").addClass("in");
+                    window.setTimeout(function() { $("#err-pass").alert('close'); }, 2000); 
 	            
 	            return false;
 	      	}
